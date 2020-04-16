2f11c9fbbc00b61333ade620b5ad6861e388ee23
Talia Bjelland <>
2020-04-16 08:09:07 -0500

    complete game functionality + new tokens

--- public/js/main.js
+++ public/js/main.js
@@ -279,6 +279,7 @@ var old_board = [
                 ];
 
 var my_color = ' ';
+var interval_timer;
 
 socket.on('game_update',function(payload){
   console.log('*** Client Log Message: \'game_update\'\n\t payload: '+JSON.stringify(payload));
@@ -309,6 +310,24 @@ socket.on('game_update',function(payload){
   }
 
   $('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3');
+  $('#my_color').append('<h4> It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>');
+
+  clearInterval(interval_timer);
+  interval_timer = setInterval(function(last_time){
+                return function(){
+                  //Do the work of updating the UI
+                  var d = new Date();
+                  var elapsedmilli = d.getTime() - last_time;
+                  var minutes = Math.floor(elapsedmilli / (60*1000));
+                  var seconds = Math.floor((elapsedmilli % (60*1000))/1000);
+
+                  if(seconds<10){
+                    $('#elapsed').html(minutes+':0'+seconds);
+                  }
+                  else{
+                    $('#elapsed').html(minutes+':'+seconds);
+                  }
+                }}(payload.game.last_move_time),1000);
 
   /*Animate changes to the board*/
   var blacksum = 0;
@@ -330,27 +349,32 @@ socket.on('game_update',function(payload){
         if(old_board[row][column] == '?' && board[row][column] == ' '){
           $('#'+row+'_'+column).html('<img src="assets/images/EMPTYGIF.gif" alt="empty square" />');
         } else if(old_board[row][column] == '?' && board[row][column] == 'w'){
-          $('#'+row+'_'+column).html('<img src="assets/images/EMPTYTOWHITE.gif" alt="white square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXEMPTYTOWHITE.gif" alt="white square" />');
         } else if(old_board[row][column] == '?' && board[row][column] == 'b'){
-          $('#'+row+'_'+column).html('<img src="assets/images/EMPTYTOBLUE.gif" alt="black square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXEMPTYTOBLUE.gif" alt="black square" />');
         } else if(old_board[row][column] == ' ' && board[row][column] == 'w'){
-          $('#'+row+'_'+column).html('<img src="assets/images/EMPTYTOWHITE.gif" alt="white square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXEMPTYTOWHITE.gif" alt="white square" />');
         } else if(old_board[row][column] == ' ' && board[row][column] == 'b'){
-          $('#'+row+'_'+column).html('<img src="assets/images/EMPTYTOBLUE.gif" alt="black square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXEMPTYTOBLUE.gif" alt="black square" />');
         } else if(old_board[row][column] == 'w' && board[row][column] == ' '){
-          $('#'+row+'_'+column).html('<img src="assets/images/WHITETOEMPTY.gif" alt="empty square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXWHITETOEMPTY.gif" alt="empty square" />');
         } else if(old_board[row][column] == 'b' && board[row][column] == ' '){
-          $('#'+row+'_'+column).html('<img src="assets/images/BLUETOEMPTY.gif" alt="empty square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXBLUETOEMPTY.gif" alt="empty square" />');
         } else if(old_board[row][column] == 'w' && board[row][column] == 'b'){
-          $('#'+row+'_'+column).html('<img src="assets/images/WHITETOBLUE.gif" alt="black square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXWHITETOBLUE.gif" alt="black square" />');
         } else if(old_board[row][column] == 'b' && board[row][column] == 'w'){
-          $('#'+row+'_'+column).html('<img src="assets/images/BLUETOWHITE.gif" alt="white square" />');
+          $('#'+row+'_'+column).html('<img src="assets/images/XXBLUETOWHITE.gif" alt="white square" />');
         } else {
           $('#'+row+'_'+column).html('<img src="assets/images/ERROR.gif" alt="error" />');
         }
-        /* Set up interactivity */
-        $('#'+row+'_'+column).off('click');
-        if(board[row][column] == ' '){
+    }
+
+    /* Set up interactivity */
+    $('#'+row+'_'+column).off('click');
+    $('#'+row+'_'+column).removeClass('hovered_over');
+
+    if(payload.game.whose_turn === my_color){
+      if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){
           $('#'+row+'_'+column).addClass('hovered_over');
           $('#'+row+'_'+column).click(function(r,c){
                     return function(){
@@ -363,10 +387,6 @@ socket.on('game_update',function(payload){
                     };
           }(row,column));
         }
-        else{
-          $('#'+row+'_'+column).removeClass('hovered_over');
-        }
-
       }
     }
   }
